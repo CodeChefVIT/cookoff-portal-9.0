21ebea3f41fe5e5d372fcd556e2d104d5e370206
Nitinthegreat <nitinpandey1304@gmail.com>
2024-09-16 22:26:52 +0530

    Navbar added to page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,20 @@
 'use client';
+import NavBar from '@/components/navBar/NavBar';
 import ProfileComponent from '@/components/profileComponent';
 import DashboardComponent from '../../components/ui/dashboard';
-
 const Dashboard = () => {
   return (
     <>
+      <NavBar />
       <div className="parent bg-zinc-900 min-h-screen flex flex-col justify-between overflow-y-hidden">
         <div className="flex gap-12 mb-10">
           <div className="wrapper flex-grow">
-            <div className="mt-44 mb-14">
+            <div className="mt-24 mb-14">
               <DashboardComponent />
             </div>
             <DashboardComponent />
           </div>
-          <div className="relative right-16 top-44">
+          <div className="relative right-16 top-24">
             <ProfileComponent />
           </div>
         </div>
